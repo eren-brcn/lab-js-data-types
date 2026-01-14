37c1c78ecde1a491fb59b11f48c29242f8655de4
eren-brcn <sylar144814@gmail.com>
2026-01-14 18:46:43 +0300

    Solved LabEREN

--- index.js
+++ index.js
@@ -9,12 +9,10 @@ const s5 = "and";
 
 // Concatenate the string variables into one new string
 
-
+const tongueTwister = `${s1} ${s2} ${s3} ${s4} ${s5} ${s3} ${s2} ${s1} ${s4}`;
 // Print out the concatenated string
 
-
-
-
+console.log(tongueTwister);
 /*******************************************
     Iteration 1.2 | Camel Tail
 *******************************************/
@@ -22,6 +20,14 @@ const part1 = "java";
 const part2 = "script";
 
 // Convert the last letter of part1 and part2 to uppercase and concatenate the strings
+const word1 = part1.slice(0, -1) + part1.slice(-1).toUpperCase();
+part1.slice(-1).toUpperCase();
+const word2 = part2.slice(0, -1) + part2.slice(-1).toUpperCase();
+part2.slice(-1).toUpperCase();
+const result = part1 + part2.slice(0, -1) + part2.slice(-1).toUpperCase();
+ 
+console.log(result);
+
 
 
 // Print the cameLtaiL-formatted string
@@ -33,6 +39,9 @@ const part2 = "script";
     Iteration 2.1 | Calculate Tip
 *******************************************/
 const billTotal = 84;
+const tipAmount = billTotal * 0.15;
+console.log(tipAmount);
+
 
 // Calculate the tip (15% of the bill total)
 
@@ -47,6 +56,8 @@ const billTotal = 84;
 *******************************************/
 
 // Generate a random integer between 1 and 10 (inclusive)
+const randomNumber = Math.floor(Math.random() * 10) + 1;
+console.log(randomNumber);
 
 
 // Print the generated random number
@@ -74,3 +85,13 @@ const expression5 = !a || !b;
 const expression6 = !(a || b);
 
 const expression7 = a && a;
+
+
+
+console.log(a && b);
+console.log(a || b);
+console.log(!a && b);
+console.log(!(a && b));
+console.log(!a || !b);
+console.log(!(a || b));
+console.log(a && a);
